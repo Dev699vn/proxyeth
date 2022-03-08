023d128d8de26b4059a042792af3b9370e964fb5
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-08 13:27:50 +0700

    Update addcron.sh

--- addcron.sh
+++ addcron.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 crontab -r
-rm -rf cronjobcmd.txt
 
-tee -a cronjobcmd.txt <<EOF
-* * * * * cd /root/eth-proxy/ && sh cronjob.sh
-EOF
-
-cron="* * * * * cd /home/root && ./cron.sh"
+cron="* * * * * cd /root/eth-proxy && ./cron.sh"
 (crontab -u root -l; echo "$cron" ) | crontab -u root -
 
