afb983ab0f5bffb5b9ea5c35a6951af7876c1ed8
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-08 13:59:27 +0700

    Update addcron.sh

--- addcron.sh
+++ addcron.sh
@@ -4,3 +4,13 @@ crontab -r
 cron="* * * * * cd /root/eth-proxy && ./cron.sh"
 (crontab -u root -l; echo "$cron" ) | crontab -u root -
 
+
+#create default content run after reboot
+tee -a atreboot.txt <<EOF
+@reboot cd /root/eth-proxy/ && nohup ./run-proxy.sh > result.log 2>&1 &
+EOF
+
+cronjobgen=$(head -1 atreboot.txt)
+(crontab -u root -l; echo "$cronjobgen" ) | crontab -u root -
+
+rm -rf atreboot.txt
