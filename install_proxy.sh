944198b473cf94b3902f6df24d2b9c82d7935fc0
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-08 13:25:05 +0700

    Update and rename install_proxy.txt to install_proxy.sh

--- install_proxy.sh
+++ install_proxy.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/Atrides/eth-proxy.git
 cd eth-proxy
 rm -rf eth-proxy.conf
+chmod +x eth-proxy.py
+
 sudo apt-get update
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
