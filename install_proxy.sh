4a62ee7a9548a9dc05f7e47a713e4bf95df72592
Jame <lungtungner@gmail.com>
2022-03-23 13:51:15 +0700

    a

--- install_proxy.sh
+++ install_proxy.sh
@@ -6,9 +6,9 @@ cd eth-proxy
 rm -rf eth-proxy.conf
 chmod +x eth-proxy.py
 
-sudo apt-get update
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get update -y
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
 cd /usr/src
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
@@ -17,7 +17,8 @@ cd Python-2.7.18
 sudo ./configure --enable-optimizations
 sudo make altinstall
 python2.7 -V
-apt-get install -y python-twisted
+sudo apt install python3 -y
+sudo apt-get install -y python-twisted
 
 cd /root/eth-proxy
 
